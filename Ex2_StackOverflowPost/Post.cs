098487de7c33268d8896043c80d89138f1433261
Rowen <rowen.simpson@wide-blue.com>
2019-08-06 14:04:41 +0100

    ex2 initial commit

---
+++ Ex2_StackOverflowPost/Post.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ex2_StackOverflowPost
+{
+    class Post
+    {
+        public string _title { get; private set; }
+        public string _description { get; private set; }
+        public DateTime _date { get; private set; }
+        public int _voteCount { get; private set; }
+
+        public Post(string title, string description)
+        {
+            this._title = title;
+            this._description = description;
+            this._date = DateTime.Now;
+            this._voteCount = 0;
+        }
+
+        public void Upvote()
+        {
+            this._voteCount++;
+        }
+
+        public void Downvote()
+        {
+            this._voteCount--;
+        }
+
+        public int SeeVoteValue()
+        {
+            return this._voteCount;
+        }
+    }
+}
---
+++ Ex2_StackOverflowPost/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ex2_StackOverflowPost
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var post = new Post("Concerning Bob","Bob Canny Code for shizzle");
+
+            for (int i = 0; i <= 9; i++)
+                post.Upvote();
+
+            for (int i = 0; i <= 4; i++)
+                post.Downvote();
+
+            Console.WriteLine(post.SeeVoteValue());            
+
+            Console.ReadLine();
+        }
+    }
+}
